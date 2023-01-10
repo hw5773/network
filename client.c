dc61d2398fcfad74ca9d13e45ada4a3ec5ea316f
Hyunwoo Lee <hw5773@gmail.com>
2023-01-10 17:07:51 +0900

    update the C codes

--- client.c
+++ client.c
@@ -4,34 +4,98 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <getopt.h>
+#include <stdint.h>
+
 void error_handling(char *message);
+
+void usage(const char *pname)
+{
+  printf(">> Usage: %s [options]\n", pname);
+  printf("Options\n");
+  printf("  -a, --addr       Server's address\n");
+  printf("  -p, --port       Server's port\n");
+  exit(0);
+}
 
 int main(int argc, char *argv[])
 {
 	int sock;
 	struct sockaddr_in serv_addr;
-  char *msg = "hello, server!\n";
+  char *msg = "Hello, World!\n";
 	char message[30];
-	int str_len;
+	int c, port, tmp, str_len;
+  uint8_t *pname, *addr;
+  uint8_t eflag;
 
-	if (argc != 3)
-	{
-		printf("Usage: %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
+  pname = argv[0];
+  addr = NULL;
+  port = -1;
+  eflag = 0;
+
+  while (1)
+  {
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"addr", required_argument, 0, 'a'},
+      {"port", required_argument, 0, 'p'},
+      {0, 0, 0, 0}
+    };
+
+    const char *opt = "a:p:0";
+
+    c = getopt_long(argc, argv, opt, long_options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c)
+    {
+      case 'a':
+        tmp = strlen(optarg);
+        addr = (uint8_t *)malloc(tmp);
+        memcpy(addr, optarg, tmp);
+        break;
+
+      case 'p':
+        port = atoi(optarg);
+        break;
+
+      default:
+        usage(pname);
+    }
+  }
+
+  if (!addr)
+  {
+    printf("[*] Please specify the server's address to connect\n");
+    eflag = 1;
+  }
+
+  if (port < 0)
+  {
+    printf("[*] Please specify the server's port to connect\n");
+    eflag = 1;
+  }
+
+  if (eflag)
+  {
+    usage(pname);
+    exit(0);
+  }
 
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (sock == -1)
 		error_handling("socket() error");
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+	serv_addr.sin_addr.s_addr = inet_addr(addr);
+	serv_addr.sin_port = htons(port);
 
 	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error");
 	
-  printf("connect() success\n");
+  printf("[*] Connected to %s:%d\n", addr, port);
   str_len = write(sock, msg, strlen(msg));
 
   if (str_len == -1)
@@ -42,7 +106,7 @@ int main(int argc, char *argv[])
 	if (str_len == -1)
 		error_handling("read() error");
 	
-	printf("Message from server: %s \n", message);
+	printf("[*] Message from server: %s \n", message);
 	close(sock);
 	return 0;
 }
--- server.c
+++ server.c
@@ -4,26 +4,75 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <getopt.h>
+#include <stdint.h>
 
 void error_handling(char *message);
+
+void usage(const char *pname)
+{
+  printf(">> Usage: %s [options]\n", pname);
+  printf("Options\n");
+  printf("  -p, --port     Server's opening port\n");
+  exit(0);
+}
 
 int main(int argc, char *argv[])
 {
 	int serv_sock;
 	int clnt_sock;
+  const char *pname;
+  uint8_t eflag;
+  int c, port, tmp;
 
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size;
 
-	char message[] = "Hello World!";
+	char message[] = "Hi! Client!";
   char buf[256] = {0, };
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <port>\n", argv[0]);
-		exit(1);
-	}
+  port = -1;
+  eflag = 0;
+  pname = argv[0];
+
+  while (1)
+  {
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"port", required_argument, 0, 'p'},
+      {0, 0, 0, 0}
+    };
+
+    const char *opt = "p:0";
+
+    c = getopt_long(argc, argv, opt, long_options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c)
+    {
+      case 'p':
+        port = atoi(optarg);
+        break;
+
+      default:
+        usage(pname);
+    }
+  }
+
+  if (port < 0)
+  {
+    printf("Please specify the server's opening port\n");
+    eflag = 1;
+  }
+
+  if (eflag)
+  {
+    usage(pname);
+    exit(1);
+  }
 
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (serv_sock == -1)
@@ -32,7 +81,7 @@ int main(int argc, char *argv[])
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(atoi(argv[1]));
+	serv_addr.sin_port = htons(port);
 
 	if (bind(serv_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("bind() error");
@@ -41,15 +90,21 @@ int main(int argc, char *argv[])
 		error_handling("listen() error");
 	
 	clnt_addr_size = sizeof(clnt_addr);
-	clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
-	if (clnt_sock == -1)
-		error_handling("accept() error");
 
-  read(clnt_sock, buf, sizeof(buf));
-  printf("Message from client: %s\n", buf);
+  printf("[*] Server is listening on 0.0.0.0:%d\n", port);
+  while (1)
+  {
+	  clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock)
+    {
+      //printf("[*] Accept a client from %s:%d\n", );
+      read(clnt_sock, buf, sizeof(buf));
+      printf("[*] Message from client: %s\n", buf);
 
-	write(clnt_sock, message, sizeof(message));
-	close(clnt_sock);
+  	  write(clnt_sock, message, sizeof(message));
+  	  close(clnt_sock);
+    }
+  }
 	close(serv_sock);
 
 	return 0;
